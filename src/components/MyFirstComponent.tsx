bd53cfdd0290c2bc3396511c6cfaaf5cea2eef24
Manu Dragomir <dragomir.manualex@gmail.com>
2020-10-21 11:26:44 +0300

    first commit

---
+++ src/components/MyFirstComponent.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './MyFirstComponent.css';
+
+const MyFirstComponent: React.FC = () => {
+  return (
+    <div className="container">
+        <p>Prima mea componenta din Ionic. :) </p>
+    </div>
+  );
+};
+
+export default MyFirstComponent;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+import MyFirstComponent from '../components/MyFirstComponent';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -8,16 +8,16 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Plant App</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Plant App</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <MyFirstComponent />
       </IonContent>
     </IonPage>
   );
